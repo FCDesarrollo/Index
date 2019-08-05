cc7467d005315f359898fae3058602b1c1f77ad8
Arturo Gallegos Ortega <jgallegosortega@gmail.com>
2019-08-05 12:27:32 -0600

    Creación de Inicio y Clases

---
+++ Index/Clases/clDocumento.vb
@@ -0,0 +1,102 @@
+﻿Public Class clDocumento
+    Private _dFolio As Double
+    Private _dNumMoneda As Long
+    Private _dTipoCambio As Double
+    Private _dImporte As Double       ' Importe se asigna solamente cuando se va crear un documento de CxC o CxP, en otro su valor es cero.
+    Private _dCodConcepto As String
+    Private _dSerie As String
+    Private _dFecha As String   ' Formato mm/dd/aaaa. Las diagonales "/" son parte del formato
+    Private _dCodigoCteProv As String
+    Private _dCodigoAgente As String
+    Private _dReferencia As String
+
+    Public Property DFolio As Double
+        Get
+            Return _dFolio
+        End Get
+        Set(value As Double)
+            _dFolio = value
+        End Set
+    End Property
+
+    Public Property DNumMoneda As Long
+        Get
+            Return _dNumMoneda
+        End Get
+        Set(value As Long)
+            _dNumMoneda = value
+        End Set
+    End Property
+
+    Public Property DTipoCambio As Double
+        Get
+            Return _dTipoCambio
+        End Get
+        Set(value As Double)
+            _dTipoCambio = value
+        End Set
+    End Property
+
+    Public Property DImporte As Double
+        Get
+            Return _dImporte
+        End Get
+        Set(value As Double)
+            _dImporte = value
+        End Set
+    End Property
+
+    Public Property DCodConcepto As String
+        Get
+            Return _dCodConcepto
+        End Get
+        Set(value As String)
+            _dCodConcepto = value
+        End Set
+    End Property
+
+    Public Property DSerie As String
+        Get
+            Return _dSerie
+        End Get
+        Set(value As String)
+            _dSerie = value
+        End Set
+    End Property
+
+    Public Property DFecha As String
+        Get
+            Return _dFecha
+        End Get
+        Set(value As String)
+            _dFecha = value
+        End Set
+    End Property
+
+    Public Property DCodigoCteProv As String
+        Get
+            Return _dCodigoCteProv
+        End Get
+        Set(value As String)
+            _dCodigoCteProv = value
+        End Set
+    End Property
+
+    Public Property DCodigoAgente As String
+        Get
+            Return _dCodigoAgente
+        End Get
+        Set(value As String)
+            _dCodigoAgente = value
+        End Set
+    End Property
+
+    Public Property DReferencia As String
+        Get
+            Return _dReferencia
+        End Get
+        Set(value As String)
+            _dReferencia = value
+        End Set
+    End Property
+End Class
---
+++ Index/Formularios/frmPrincipal.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmPrincipal))
+        Me.tMenu = New System.Windows.Forms.ToolStrip()
+        Me.MConfig = New System.Windows.Forms.ToolStripButton()
+        Me.tMenu.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tMenu
+        '
+        Me.tMenu.AutoSize = False
+        Me.tMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MConfig})
+        Me.tMenu.Location = New System.Drawing.Point(0, 0)
+        Me.tMenu.Name = "tMenu"
+        Me.tMenu.Size = New System.Drawing.Size(800, 62)
+        Me.tMenu.TabIndex = 6
+        Me.tMenu.Text = "tMenu"
+        '
+        'MConfig
+        '
+        Me.MConfig.AutoSize = False
+        Me.MConfig.Image = CType(resources.GetObject("MConfig.Image"), System.Drawing.Image)
+        Me.MConfig.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.MConfig.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.MConfig.Name = "MConfig"
+        Me.MConfig.Size = New System.Drawing.Size(85, 60)
+        Me.MConfig.Tag = "-1"
+        Me.MConfig.Text = "Configuración"
+        Me.MConfig.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.tMenu)
+        Me.Name = "frmPrincipal"
+        Me.Text = "Sistema Index"
+        Me.tMenu.ResumeLayout(False)
+        Me.tMenu.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tMenu As ToolStrip
+    Friend WithEvents MConfig As ToolStripButton
+End Class
